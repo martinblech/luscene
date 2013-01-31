93a17f84f6d110f9e466f87f85c295b242508a80
Martin Blech <martinblech@gmail.com>
2013-01-30 22:31:29 -0300

    precalculate all field types (instead of creating them on the fly)

--- src/main/scala/luscene/DocumentBuilder.scala
+++ src/main/scala/luscene/DocumentBuilder.scala
@@ -18,25 +18,69 @@ trait DocumentBuilder {
   }
 
   def mkField(name: String, value: Any): IndexableField = {
-    def fieldType(ref: FieldType) = {
-      val ft = new FieldType(ref)
-      ft.setStored(fieldStore(name))
-      ft.setIndexed(fieldIndex(name))
-      ft
+    def fieldType(cls: Class[_ <: IndexableField]) = {
+      val stored = fieldStore(name)
+      val indexed = fieldIndex(name)
+      DocumentBuilder.fieldTypeMap((cls, stored, indexed))
     }
     value match {
       case s: String =>
-        new Field(name, s, fieldType(TextField.TYPE_STORED))
+        new Field(name, s, fieldType(classOf[TextField]))
       case i: Int =>
-        new IntField(name, i, fieldType(IntField.TYPE_STORED))
+        new IntField(name, i, fieldType(classOf[IntField]))
       case l: Long =>
-        new LongField(name, l, fieldType(LongField.TYPE_STORED))
+        new LongField(name, l, fieldType(classOf[LongField]))
       case f: Float =>
-        new FloatField(name, f, fieldType(FloatField.TYPE_STORED))
+        new FloatField(name, f, fieldType(classOf[FloatField]))
       case d: Double =>
-        new DoubleField(name, d, fieldType(DoubleField.TYPE_STORED))
+        new DoubleField(name, d, fieldType(classOf[DoubleField]))
       case _ => throw new IllegalArgumentException(
         "don't know how to index '%s' (%s)".format(value, value.getClass))
     }
   }
+}
+
+object DocumentBuilder {
+  val fieldTypeMap: Map[(Class[_], Boolean, Boolean), FieldType] = Map(
+    // TextField
+    (classOf[TextField], true, true) -> TextField.TYPE_STORED,
+    (classOf[TextField], true, false) -> {
+      val ft = new FieldType(TextField.TYPE_STORED)
+      ft.setIndexed(false)
+      ft
+    },
+    (classOf[TextField], false, true) -> TextField.TYPE_NOT_STORED,
+    // IntField
+    (classOf[IntField], true, true) -> IntField.TYPE_STORED,
+    (classOf[IntField], true, false) -> {
+      val ft = new FieldType(IntField.TYPE_STORED)
+      ft.setIndexed(false)
+      ft
+    },
+    (classOf[IntField], false, true) -> IntField.TYPE_NOT_STORED,
+    // LongField
+    (classOf[LongField], true, true) -> LongField.TYPE_STORED,
+    (classOf[LongField], true, false) -> {
+      val ft = new FieldType(LongField.TYPE_STORED)
+      ft.setIndexed(false)
+      ft
+    },
+    (classOf[LongField], false, true) -> LongField.TYPE_NOT_STORED,
+    // FloatField
+    (classOf[FloatField], true, true) -> FloatField.TYPE_STORED,
+    (classOf[FloatField], true, false) -> {
+      val ft = new FieldType(FloatField.TYPE_STORED)
+      ft.setIndexed(false)
+      ft
+    },
+    (classOf[FloatField], false, true) -> FloatField.TYPE_NOT_STORED,
+    // DoubleField
+    (classOf[DoubleField], true, true) -> DoubleField.TYPE_STORED,
+    (classOf[DoubleField], true, false) -> {
+      val ft = new FieldType(DoubleField.TYPE_STORED)
+      ft.setIndexed(false)
+      ft
+    },
+    (classOf[DoubleField], false, true) -> DoubleField.TYPE_NOT_STORED
+  )
 }
